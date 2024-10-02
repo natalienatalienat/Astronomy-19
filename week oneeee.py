b4105bf2cbb4abdc153b141acc137d11f28d3028
Natalie Pile <npile@ucsc.edu>
2024-10-02 14:19:05 -0700

    Add files via upload

---
+++ week oneeee.py
@@ -0,0 +1,13 @@
+#this program will write my first journal entry
+
+#this function tells the computer to print my name pronouns and two sentences
+def Printweekone():
+    print("My name is Natalie Pile. My pronouns are she and her. My favorite movie is Iron Man. My favorite food is vodka pasta.")
+
+    #this defines out main() function for our program
+    def main():
+        Printweekone()
+
+    #when we run the pro0gram this goes first
+    if __name__ == '__main__':
+         main()
